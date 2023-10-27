fbfff2ce092718ac4199bc181093e0548858a95d
Abdul Wasay <abdulwaseybutt@gmail.com>
2023-10-27 16:14:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// All pages
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import DemoProduct from "./pages/DemoProduct";
